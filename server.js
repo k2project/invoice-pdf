394d4a2b0e916965f6d8a64757c534f1e20ce1a7
k2projct <kopczkrzy@gmail.com>
2020-03-03 18:10:25 +0000

    server routes setup

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 connectDB();
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 app.listen(process.env.PORT || 5000, () => console.log('server is running'));
