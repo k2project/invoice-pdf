9fdd7b2ff9e8fe03a27053c37268605810f9838a
k2projct <kopczkrzy@gmail.com>
2020-03-03 18:17:59 +0000

    user schema defined

---
+++ models/Users.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
