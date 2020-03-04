e55acf2144090067a4691e2431346309ef35ba03
k2projct <kopczkrzy@gmail.com>
2020-03-04 09:46:46 +0000

    encripting password and creating token in user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,62 @@
 const express = require('express');
 const router = express.Router();
-//@route    GET api/users
-//@desc     Users route
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+//@route    POST api/users
+//@desc     Register user
 //@status   Public
-router.get('/', (req, res) => res.send('Users route'));
+router.post(
+    '/',
+    [
+        check('email', 'Please enter a valid email address.').isEmail(),
+        check('password')
+            .isLength({ min: 5 })
+            .withMessage('must be at least 5 chars long')
+            .matches(/\d/)
+            .withMessage('must contain a number')
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ erorrs: errors.array() });
+        }
+        const { email, password } = req.body;
+        try {
+            //check if user exists
+            let user = await User.findOne({ email });
+            if (user) {
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'User already exists.' }] });
+            }
+            user = new User({
+                email,
+                password
+            });
+            //encrypt password
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+            //save a new user to DB
+            await user.save();
+            //asign a token
+            const payload = { user: { id: user.id } };
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 3600 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+        } catch (err) {
+            console.log(err.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 connectDB();
-
+app.use(express.json({ extended: false }));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 
