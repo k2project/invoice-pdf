2e1598ddc7500ed18dce27f5ba846f25974a41e0
k2projct <kopczkrzy@gmail.com>
2020-03-11 21:02:28 +0000

    delete acount component

--- client/src/components/layout/Profile/Settings/DeleteAccount.js
+++ client/src/components/layout/Profile/Settings/DeleteAccount.js
@@ -1,27 +1,61 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 const DeleteAccount = props => {
-    function handleClick(e) {
+    const [deleteConfirmation, setDeleteConfirmation] = useState(false);
+    async function handleDelete(e) {
         e.preventDefault();
+        await setDeleteConfirmation(true);
+        document.getElementById('password').focus();
     }
-    function onEnter(e) {
-        if (e.keyCode === 13) handleClick(e);
+    function deleteOnEnter(e) {
+        if (e.keyCode === 13) handleDelete(e);
+    }
+    function handleCancelation() {
+        setDeleteConfirmation(false);
+    }
+    function cancelationOnEnter(e) {
+        if (e.keyCode === 13) handleCancelation(e);
     }
     return (
-        <section>
+        <section className='delete-account'>
             <h2 className='heading heading--sml'>Delete Account.</h2>
             <p>
                 Once you delete your account, there is no going back. Please be
                 certain.
             </p>
-            <button
-                className='btn btn--danger'
-                onMouseDown={handleClick}
-                onKeyDown={onEnter}
-            >
-                Delete Account
-            </button>
+            <form>
+                {deleteConfirmation && (
+                    <Fragment>
+                        <label htmlFor='password'>
+                            Enter password to confirm the deactivation of your
+                            account.
+                        </label>
+                        <input
+                            type='password'
+                            name='password'
+                            id='password'
+                            className='form__input form__input--sml'
+                        />
+                    </Fragment>
+                )}
+                {deleteConfirmation && (
+                    <button
+                        className='btn btn--grey btn--sibling'
+                        onMouseDown={handleCancelation}
+                        onKeyDown={cancelationOnEnter}
+                    >
+                        Cancel
+                    </button>
+                )}
+                <button
+                    className='btn btn--danger'
+                    onMouseDown={handleDelete}
+                    onKeyDown={deleteOnEnter}
+                >
+                    Delete Account
+                </button>
+            </form>
         </section>
     );
 };
